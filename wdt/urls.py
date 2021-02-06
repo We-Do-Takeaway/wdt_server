5437a544553c012d17d526f070325ee30bd47e8c
Zac Tolley <zac@thetolleys.com>
2021-02-06 14:14:37 +0000

    Fix deprecation warning in urls

--- wdt/urls.py
+++ wdt/urls.py
@@ -1,13 +1,12 @@
 from ariadne.contrib.django.views import GraphQLView
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import RedirectView
 
 from wdt.schema.schema import schema
 
 urlpatterns = [
-    url(r"^favicon\.ico$", RedirectView.as_view(url="/static/images/favicon.ico")),
+    re_path(r"^favicon\.ico$", RedirectView.as_view(url="/static/images/favicon.ico")),
     path("admin/", admin.site.urls),
     path("graphql/", GraphQLView.as_view(schema=schema), name="graphql"),
 ]
