ad1261f56bacab2a9a3869f039b979e8f006a94f
Zac Tolley <zac@thetolleys.com>
2021-02-06 11:03:33 +0000

    Add tests for add item to basket

--- wdt/basket/resolvers/basket_mutations.py
+++ wdt/basket/resolvers/basket_mutations.py
@@ -1,12 +1,20 @@
 from ariadne import ObjectType, convert_kwargs_to_snake_case
+from django.db import transaction
 
 from wdt.basket.models import Basket, BasketItem
+from wdt.menu.models import Item
+from wdt.shared.exceptions import InvalidItemError
 
 basket_mutation = ObjectType("Mutation")
+
+ITEM_DOESNT_EXIST = "The item id does not exist"
+TOO_MANY_ITEMS = "Too many items added"
+MAX_ITEMS_ALLOWED = 50
 
 
 @basket_mutation.field("addBasketItem")
 @convert_kwargs_to_snake_case
+@transaction.atomic
 def resolve_add_basket_item(obj, info, *, basket_id, basket_item):
     # Check if there is a basket to add to, if not then remove it
     try:
@@ -15,24 +23,38 @@ def resolve_add_basket_item(obj, info, *, basket_id, basket_item):
         basket = Basket(id=basket_id)
         basket.save()
 
-    basket_item = BasketItem(
-        basket_id=basket_id,
-        item_id=basket_item["item_id"],
-        quantity=basket_item["quantity"],
-    )
+    try:
+        Item.objects.get(pk=basket_item["item_id"])
+    except Item.DoesNotExist:
+        raise InvalidItemError(ITEM_DOESNT_EXIST)
 
-    basket_item.save()
+    # Check if there is an entry for this item already and update it, otherwise add a new one
+    try:
+        db_basket_item = BasketItem.objects.get(item_id=basket_item["item_id"], basket_id=basket_id)
+        db_basket_item.quantity += basket_item["quantity"]
+    except BasketItem.DoesNotExist:
+        db_basket_item = BasketItem(
+            basket_id=basket_id,
+            item_id=basket_item["item_id"],
+            quantity=basket_item["quantity"],
+        )
+
+    if db_basket_item.quantity > MAX_ITEMS_ALLOWED:
+        raise InvalidItemError(TOO_MANY_ITEMS)
+
+    db_basket_item.save()
 
     return basket
 
 
 @basket_mutation.field("updateBasketItem")
 @convert_kwargs_to_snake_case
+@transaction.atomic
 def resolve_update_basket_item(obj, info, *, basket_id, basket_item):
     # Check if there is a basket to add to, if not then remove it
     try:
         basket = Basket.objects.get(pk=basket_id)
-        old_item = BasketItem.objects.get(item_id=basket_item.item_id)
+        old_item = BasketItem.objects.get(item_id=basket_item["item_id"], basket_id=basket_id)
         old_item.delete()
     except Basket.DoesNotExist:
         return None
@@ -51,6 +73,7 @@ def resolve_update_basket_item(obj, info, *, basket_id, basket_item):
 
 @basket_mutation.field("removeBasketItem")
 @convert_kwargs_to_snake_case
+@transaction.atomic
 def resolve_remove_basket_item(obj, info, *, basket_id, item_id):
     # Delete the item then fetch the basket
     try:
@@ -65,6 +88,7 @@ def resolve_remove_basket_item(obj, info, *, basket_id, item_id):
 
 @basket_mutation.field("clearBasket")
 @convert_kwargs_to_snake_case
+@transaction.atomic
 def resolve_clear_basket(obj, info, *, basket_id):
     # Check if there is a basket to add to, if not then remove it
     try:
--- wdt/basket/tests/test_add_basket_item.py
+++ wdt/basket/tests/test_add_basket_item.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 
 import pytest
 
+from wdt.basket.models import BasketItem
+
 MUTATION_QUERY = """
 mutation AddBasketItem(
     $basketId: ID!,
@@ -21,31 +23,34 @@ mutation AddBasketItem(
 }
 """
 
+EXISTING_BASKET_ID = "b63920b5-d2ca-4e5a-a7c5-05b7cbf00cc3"
+NEW_BASKET_ID = "b63920b5-d2ca-4e5a-a7c5-05b7cbf00cc4"
+SAUSAGES_ID = "a4b463c9-f786-4945-af19-fb3103d3984b"
+CHERRIES_ID = "124cec9c-0352-48a4-835f-5c4e3d45f69b"
+INVALID_ITEM_ID = "124cec9c-0352-48a4-835f-5c4e3d45f69c"
+
 
 @pytest.mark.django_db
 @pytest.mark.usefixtures("example_data")
 class TestAddBasketItem:
     def test_add_valid_item_to_existing_basket(self, graphql_request):
         variables = {
-            "basketId": "b63920b5-d2ca-4e5a-a7c5-05b7cbf00cc3",
+            "basketId": EXISTING_BASKET_ID,
             "basketItem": {
-                "itemId": "a4b463c9-f786-4945-af19-fb3103d3984b",
+                "itemId": SAUSAGES_ID,
                 "quantity": 1
             }
         }
 
         response = graphql_request(MUTATION_QUERY, variables=variables)
-
-        response_data = response.json()["data"]
-
         assert response.status_code == HTTPStatus.OK
-
+        response_data = response.json()["data"]
         assert response_data == {
             "addBasketItem": {
-                "id": "b63920b5-d2ca-4e5a-a7c5-05b7cbf00cc3",
+                "id": EXISTING_BASKET_ID,
                 "items": [
                     {
-                        "id": "a4b463c9-f786-4945-af19-fb3103d3984b",
+                        "id": SAUSAGES_ID,
                         "name": "Plate of sausages",
                         "quantity": 1,
                     }
@@ -53,4 +58,127 @@ class TestAddBasketItem:
             }
         }
 
+    def test_add_valid_item_to_unknown_basket(self, graphql_request):
+        variables = {
+            "basketId": NEW_BASKET_ID,
+            "basketItem": {
+                "itemId": SAUSAGES_ID,
+                "quantity": 1
+            }
+        }
 
+        response = graphql_request(MUTATION_QUERY, variables=variables)
+        assert response.status_code == HTTPStatus.OK
+        response_data = response.json()["data"]
+        assert response_data == {
+            "addBasketItem": {
+                "id": NEW_BASKET_ID,
+                "items": [
+                    {
+                        "id": SAUSAGES_ID,
+                        "name": "Plate of sausages",
+                        "quantity": 1,
+                    }
+                ]
+            }
+        }
+
+    def test_add_invalid_item_to_existing_basket(self, graphql_request):
+        variables = {
+            "basketId": EXISTING_BASKET_ID,
+            "basketItem": {
+                "itemId": INVALID_ITEM_ID,
+                "quantity": 1
+            }
+        }
+
+        response = graphql_request(MUTATION_QUERY, variables=variables)
+        errors = response.json()["errors"]
+
+        assert errors[0]["message"] == "The item id does not exist"
+
+    def test_add_second_item_to_basket(self, graphql_request):
+        basket_item = BasketItem(
+            basket_id=EXISTING_BASKET_ID,
+            item_id=SAUSAGES_ID,
+            quantity=1,
+        )
+
+        basket_item.save()
+
+        variables = {
+            "basketId": EXISTING_BASKET_ID,
+            "basketItem": {
+                "itemId": CHERRIES_ID,
+                "quantity": 1
+            }
+        }
+
+        response = graphql_request(MUTATION_QUERY, variables=variables)
+        assert response.status_code == HTTPStatus.OK
+        response_data = response.json()["data"]
+        assert response_data == {
+            "addBasketItem": {
+                "id": EXISTING_BASKET_ID,
+                "items": [
+                    {
+                        "id": CHERRIES_ID,
+                        "name": "Bowl of cherries",
+                        "quantity": 1,
+                    },
+                    {
+                        "id": SAUSAGES_ID,
+                        "name": "Plate of sausages",
+                        "quantity": 1,
+                    },
+                ]
+            }
+        }
+
+    def test_add_duplicate_item_to_basket(self, graphql_request):
+        basket_item = BasketItem(
+            basket_id=EXISTING_BASKET_ID,
+            item_id=SAUSAGES_ID,
+            quantity=1,
+        )
+
+        basket_item.save()
+
+        variables = {
+            "basketId": EXISTING_BASKET_ID,
+            "basketItem": {
+                "itemId": SAUSAGES_ID,
+                "quantity": 1
+            }
+        }
+
+        response = graphql_request(MUTATION_QUERY, variables=variables)
+        assert response.status_code == HTTPStatus.OK
+        response_data = response.json()["data"]
+        assert response_data == {
+            "addBasketItem": {
+                "id": EXISTING_BASKET_ID,
+                "items": [
+                    {
+                        "id": SAUSAGES_ID,
+                        "name": "Plate of sausages",
+                        "quantity": 2,
+                    }
+                ]
+            }
+        }
+
+    def test_too_many_to_basket(self, graphql_request):
+        variables = {
+            "basketId": NEW_BASKET_ID,
+            "basketItem": {
+                "itemId": SAUSAGES_ID,
+                "quantity": 99
+            }
+        }
+
+        response = graphql_request(MUTATION_QUERY, variables=variables)
+        assert response.status_code == HTTPStatus.OK
+        errors = response.json()["errors"]
+
+        assert errors[0]["message"] == "Too many items added"
